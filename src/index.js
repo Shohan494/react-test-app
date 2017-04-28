a7dc6ecf19ea75d06073e13bcf466650facfd275
shohan494 <shohan12224943@gmail.com>
2017-04-29 02:35:37 +0600

    state and lifecycle part 1

--- src/index.js
+++ src/index.js
@@ -4,80 +4,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-//example of props
-/* 1st one
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
-
-const element = <Welcome name="Sara" />;
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
-*/
-
-// 2nd one
-/*
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
-
-function App() {
-  return (
-    <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
-    </div>
-  );
-}
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-*/
-
-// 3rd one
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
-
-function Comment(props) {
-  return (
-    <div className="Comment">
-      <div className="UserInfo">
-        <img className="Avatar"
-             src={props.author.avatarUrl}
-             alt={props.author.name} />
-        <div className="UserInfo-name">
-          {props.author.name}
-        </div>
-      </div>
-      <div className="Comment-text">
-        {props.text}
-      </div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-      </div>
-    </div>
-  );
-}
-
-const comment = {
-  date: new Date(),
-  text: 'I hope you enjoy learning React!',
-  author: {
-    name: 'Hello Kitty',
-    avatarUrl: 'http://placekitten.com/g/64/64'
+// same example with class
+// example of Adding Local State to a Class
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
   }
-};
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <Comment
-    date={comment.date}
-    text={comment.text}
-    author={comment.author} />,
+  <Clock />,
   document.getElementById('root')
 );
--- src/time.js
+++ src/time.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 //import App from './App';
 import './index.css';
 
+// slightly different example of timeclock
+/*
 function tick() {
   const element = (
     <div>
@@ -17,3 +19,28 @@ function tick() {
 }
 
 setInterval(tick, 1000);
+*/
+
+// same example with class
+// example of Adding Local State to a Class
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+);
+
